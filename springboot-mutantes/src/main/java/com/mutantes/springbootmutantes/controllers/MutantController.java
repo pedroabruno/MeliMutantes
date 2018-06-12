585c130b614c98a94a3c1884e6672fc2eeaef148
Pedro <Pedro@192.168.1.14>
2018-06-11 23:04:52 -0300

    asfasfas

--- springboot-mutantes/src/main/java/com/mutantes/springbootmutantes/controllers/MutantController.java
+++ springboot-mutantes/src/main/java/com/mutantes/springbootmutantes/controllers/MutantController.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +45,10 @@ public class MutantController {
 	public String getMutantStats() {
 		return mutantService.getMutantStats();
 	}
+	
+	@GetMapping("/cumbia")
+	  public String hello() {
+	    return "CUMBIA PIOLA!";
+	 }
 	
 }
